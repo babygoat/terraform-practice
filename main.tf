7a6b422236a89dc1993c95aaba5ad96be55ac40a
Ching-Yang, Tseng <n7711245566@gmail.com>
2019-09-30 00:09:43 +0800

    Add provisioner to resource

--- main.tf
+++ main.tf
@@ -19,6 +19,13 @@ resource "google_compute_instance" "vm_instance" {
   machine_type = "f1-micro"
   tags         = ["web", "dev"]
 
+  # Execute only when the resource is created
+  # Use terraform taint <resource type>.<resource name> to mark for recreation
+  # Use terraform apply to enforce the resource recreation
+  provisioner "local-exec" {
+    command = "echo ${google_compute_instance.vm_instance.name}: ${google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip} >> ip_address.txt"
+  }
+
   boot_disk {
     initialize_params {
       image = "cos-cloud/cos-stable"
