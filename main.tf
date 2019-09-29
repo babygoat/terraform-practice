457631a64330789e1c1cd0e5166da369ec0d4a62
Ching-Yang, Tseng <n7711245566@gmail.com>
2019-09-29 22:14:36 +0800

    Revert "Add explicit dependency"
    This reverts commit 3a43884399efcadf3a70b8f977d9c85bff707fed.
    
    These resources only demo the usage of `depends_on` property.

--- main.tf
+++ main.tf
@@ -41,34 +41,3 @@ resource "google_compute_address" "vm_static_ip" {
   name = "terraform-static-ip"
 }
 
-resource "google_storage_bucket" "example_bucket" {
-  name     = "terraform-example-bucket-terraform-tutorial"
-  location = "US"
-
-  website {
-    main_page_suffix = "index.html"
-    not_found_page   = "404.html"
-  }
-}
-
-resource "google_compute_instance" "another_instance" {
-  # Explicit dependency
-  # Tells terraform that this VM instance must be created only after the
-  # storage bucket has been created
-  depends_on = [google_storage_bucket.example_bucket]
-
-  name         = "terraform-instance-2"
-  machine_type = "f1-micro"
-
-  boot_disk {
-    initialize_params {
-      image = "cos-cloud/cos-stable"
-    }
-  }
-
-  network_interface {
-    network = google_compute_network.vpc_network.self_link
-    access_config {
-    }
-  }
-}
